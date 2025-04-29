41f86d8b74c7a23027a87e9328d9ea39c5ab7a41
Nicholas Lennox <30431725+NicholasLennox@users.noreply.github.com>
2025-04-29 09:34:18 +0200

    updated models to explicitly include mysql2 driver

--- models/index.js
+++ models/index.js
@@ -16,6 +16,7 @@ const sequelize = new Sequelize({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
+    dialectModule: require('mysql2'), // Had to add this for vercel
     define: {
         timestamps: false
     }
